af7bcc0002d3a829e227582ed123401580a51ef8
Giacomo Accursi <giacomo.accursi@gmail.com>
2022-03-14 11:36:22 +0100

    refactor map and filter in terms of flatmap

--- src/main/scala/u03/Lists.scala
+++ src/main/scala/u03/Lists.scala
@@ -13,33 +13,34 @@ object Lists extends App:
       case Cons(h, t) => h + sum(t)
       case _ => 0
 
-    def map[A, B](l: List[A])(mapper: A => B): List[B] = l match
-      case Cons(h, t) => Cons(mapper(h), map(t)(mapper))
-      case Nil() => Nil()
+    def map[A, B](l: List[A])(mapper: A => B): List[B] =
+      flatMap(l)(v => Cons(mapper(v), Nil()))
 
-    def filter[A](l1: List[A])(pred: A => Boolean): List[A] = l1 match
-      case Cons(h, t) if pred(h) => Cons(h, filter(t)(pred))
+    def filter[A](l1: List[A])(pred: A => Boolean): List[A] =
+      /*case Cons(h, t) if pred(h) => Cons(h, filter(t)(pred))
       case Cons(_, t) => filter(t)(pred)
-      case Nil() => Nil()
+      case Nil() => Nil()*/
+      flatMap(l1)(v => if (pred(v)) Cons(v, Nil()) else Nil())
+
 
     def drop[A](l: List[A], n: Int): List[A] = l match
-     case Cons(h,t) if n > 0 => drop(t, n - 1)
-     case Cons(h, t) => Cons(h, t)
-     case Nil() => Nil()
+    case Cons(h,t) if n > 0 => drop(t, n - 1)
+    case Cons(h, t) => Cons(h, t)
+    case Nil() => Nil()
 
     def append[A](left: List[A], right: List[A]): List[A] = left match
-      case Cons(h, t) => Cons(h, append(t, right))
-      case Nil() => right
+    case Cons(h, t) => Cons(h, append(t, right))
+    case Nil() => right
 
     def flatMap[A,B](l: List[A])(f: A => List[B]): List[B] = l match
-      case Cons(h, t) => append(f(h), flatMap(t)(f))
-      case Nil() => Nil()
+    case Cons(h, t) => append(f(h), flatMap(t)(f))
+    case Nil() => Nil()
 
 
 
-  val l = List.Cons(10, List.Cons(20, List.Cons(30, List.Nil())))
-  println(List.sum(l)) // 60
+    val l = List.Cons(10, List.Cons(20, List.Cons(30, List.Nil())))
+    println(List.sum(l)) // 60
 
-  import List.*
+    import List.*
 
-  println(sum(map(filter(l)(_ >= 20))(_ + 1))) // 21+31 = 52
+    println(sum(map(filter(l)(_ >= 20))(_ + 1))) // 21+31 = 52
